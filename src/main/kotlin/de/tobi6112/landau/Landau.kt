23626f344bd3b935bcd6fca854dcf7e134cebddb
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-21 18:47:04 +0200

    Some more project basics

--- src/main/kotlin/de/tobi6112/landau/Landau.kt
+++ src/main/kotlin/de/tobi6112/landau/Landau.kt
@@ -4,16 +4,30 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import discord4j.core.DiscordClient
+import mu.KotlinLogging
+import java.time.Duration
 
 /**
  * Main class of Landau Bot, processes CLI parameters and creates DiscordClient
  */
 class Landau : CliktCommand() {
-    private val token by option("-t", "--token", help = "Bot token", envvar = "LANDAU_BOT_TOKEN").required()
+  private val logger = KotlinLogging.logger {}
 
-    override fun run() {
-        val client = DiscordClient.create(token)
-    }
+  // CLI Options
+  private val token by option("-t", "--token", help = "Bot token", envvar = "LANDAU_BOT_TOKEN").required()
+
+  @Suppress("MAGIC_NUMBER")
+  override fun run() {
+    val client = DiscordClient.create(token)
+    val gateway = client.login()
+        .doOnSuccess { logger.info { "Client successfully logged in" } }
+        .doOnError { logger.error { "Client couldn't login" } }
+        .block(Duration.ofSeconds(15))
+
+    gateway.onDisconnect()
+        .doFinally { logger.info { "Client disconnected" } }
+        .block()
+  }
 }
 
 fun main(args: Array<String>) = Landau().main(args)
