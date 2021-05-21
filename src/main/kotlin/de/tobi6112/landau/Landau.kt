5d4afb8e8362cbee146c040303ddc61bf9cefe30
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-21 23:34:36 +0200

    Add config

--- src/main/kotlin/de/tobi6112/landau/Landau.kt
+++ src/main/kotlin/de/tobi6112/landau/Landau.kt
@@ -4,8 +4,10 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.associate
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import de.tobi6112.landau.config.Configuration
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.event.domain.message.MessageCreateEvent
 import discord4j.rest.util.AllowedMentions
 import mu.KotlinLogging
 import kotlin.system.exitProcess
@@ -18,6 +20,7 @@ class Landau : CliktCommand() {
 
   // CLI Options
   private val token by option("-t", "--token", help = "Bot token", envvar = "LANDAU_BOT_TOKEN").required()
+  private val configEnv by option("-c", "--config", help = "Configuration environment")
   private val systemProperties: Map<String, String> by option("-D").associate()
 
   @Suppress("MAGIC_NUMBER")
@@ -26,6 +29,8 @@ class Landau : CliktCommand() {
     systemProperties.entries.forEach {
       System.setProperty(it.key, it.value)
     }
+
+    val config = Configuration.getConfig(configEnv)
 
     val client = DiscordClient
         .builder(token)
@@ -43,6 +48,16 @@ class Landau : CliktCommand() {
     client.on(ReadyEvent::class.java)
         .doOnNext { logger.info { "${applicationInfo.name} is ready..." } }
         .blockFirst()
+
+    client.on(MessageCreateEvent::class.java)
+        .subscribe { event ->
+          val prefix = config.bot.command.prefix
+          val message = event.message.content.trim().split("\\s+".toRegex())[0]
+          if (message.startsWith(prefix) && message.length > prefix.length) {
+            val command = message.substringAfter(prefix)
+            logger.info { "Detected $command command" }
+          }
+        }
 
     client.onDisconnect().block()
     logger.info { "Shutting down..." }
