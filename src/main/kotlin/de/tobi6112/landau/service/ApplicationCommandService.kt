cb0b35c1f57ab49724f81cd46e0df2710a06d72f
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-26 18:16:28 +0200

    Add return types

--- src/main/kotlin/de/tobi6112/landau/service/ApplicationCommandService.kt
+++ src/main/kotlin/de/tobi6112/landau/service/ApplicationCommandService.kt
@@ -24,50 +24,78 @@ class ApplicationCommandService(
    *
    * @param commands available commands
    * @param config commands config
+   * @return map id <-> Command
    */
-  fun createCommands(commands: Iterable<AbstractCommand>, config: CommandsConfig) {
+  fun createCommands(
+      commands: Iterable<AbstractCommand>,
+      config: CommandsConfig
+  ): Map<Long, AbstractCommand> {
     // Get global enabled commands
     val globalEnabledCommands = commands.filter { config.global[it.name]?.enabled ?: true }
-    createGlobalCommands(globalEnabledCommands)
+    val globalCommands = createGlobalCommands(globalEnabledCommands)
 
     // Get guild enabled commands
     val guildEnabledCommands =
         config.guilds.mapValues { (_, cmdCfg) ->
           commands.filter { cmdCfg[it.name]?.enabled ?: true }
         }
-    guildEnabledCommands.forEach { createGuildCommands(it.key, it.value) }
+    val guildCommands: MutableMap<Long, AbstractCommand> = mutableMapOf()
+    guildEnabledCommands.forEach { guildCommands.putAll(createGuildCommands(it.key, it.value)) }
+
+    guildCommands.putAll(globalCommands)
+    return guildCommands.toMap()
   }
 
-  private fun createGlobalCommands(commands: Iterable<AbstractCommand>) {
+  @Suppress("LOCAL_VARIABLE_EARLY_DECLARATION")
+  private fun createGlobalCommands(
+      commands: Iterable<AbstractCommand>
+  ): Map<Long, AbstractCommand> {
     checkMultipleCommands(commands)
+
+    val map: MutableMap<Long, AbstractCommand> = mutableMapOf()
 
     // TODO Return Mono
     commands.distinctBy { it.name }.forEach {
       val request = it.toRequest()
-      this.applicationService
-          .createGlobalApplicationCommand(applicationId, request)
-          .doOnError { err -> logger.error(err) { "Could not create global command $it" } }
-          .doOnSuccess { logger.info { "Successfully created global command $it" } }
-          .subscribe()
+      val res =
+          this.applicationService
+              .createGlobalApplicationCommand(applicationId, request)
+              .doOnError { err -> logger.error(err) { "Could not create global command $it" } }
+              .doOnSuccess { logger.info { "Successfully created global command $it" } }
+              .block()
+
+      map[res.id().toLong()] = it
     }
+
+    return map
   }
 
-  private fun createGuildCommands(guildId: Long, commands: Iterable<AbstractCommand>) {
+  private fun createGuildCommands(
+      guildId: Long,
+      commands: Iterable<AbstractCommand>
+  ): Map<Long, AbstractCommand> {
     checkMultipleCommands(commands)
+
+    val map: MutableMap<Long, AbstractCommand> = mutableMapOf()
 
     // TODO Return Mono
     commands.distinctBy { it.name }.forEach { command ->
       val request = command.toRequest()
-      this.applicationService
-          .createGuildApplicationCommand(applicationId, guildId, request)
-          .doOnError { err ->
-            logger.error(err) { "Could not create guild command $command on guild $guildId" }
-          }
-          .doOnSuccess {
-            logger.info { "Successfully created global command $command on guild $guildId" }
-          }
-          .subscribe()
+      val res =
+          this.applicationService
+              .createGuildApplicationCommand(applicationId, guildId, request)
+              .doOnError { err ->
+                logger.error(err) { "Could not create guild command $command on guild $guildId" }
+              }
+              .doOnSuccess {
+                logger.info { "Successfully created global command $command on guild $guildId" }
+              }
+              .block()
+
+      map[res.id().toLong()] = command
     }
+
+    return map
   }
 
   private fun checkMultipleCommands(commands: Iterable<AbstractCommand>) {
