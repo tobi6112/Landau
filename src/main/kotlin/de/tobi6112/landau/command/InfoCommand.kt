f69e58b46167eeef7dfe81a96dda9df3d791189c
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-26 17:59:09 +0200

    Add command options

--- src/main/kotlin/de/tobi6112/landau/command/InfoCommand.kt
+++ src/main/kotlin/de/tobi6112/landau/command/InfoCommand.kt
@@ -1,5 +1,24 @@
 package de.tobi6112.landau.command
 
+import de.tobi6112.landau.command.core.AbstractCommand
+import de.tobi6112.landau.command.core.Choice
+import de.tobi6112.landau.command.core.Option
+import de.tobi6112.landau.command.core.OptionType
+
 /** Information command */
-class InfoCommand : Command("info", "Information about Landau") {
+class InfoCommand :
+  AbstractCommand(
+    name = "info",
+    description = "Retrieve information",
+    options =
+        listOf(
+            Option(
+              type = OptionType.STRING,
+              name = "subject",
+              description = "The subject to retrieve information about",
+              required = true,
+              choices =
+                  listOf(
+                      Choice("bot", "Information about the bot"),
+                      Choice("server", "Information about the server"))))) {
 }
