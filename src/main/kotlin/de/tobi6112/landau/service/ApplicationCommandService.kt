2e40d42aa64c6e86e7ff04a568e7b59652f3561b
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-05-29 00:05:43 +0200

    Use reactive streams

--- src/main/kotlin/de/tobi6112/landau/service/ApplicationCommandService.kt
+++ src/main/kotlin/de/tobi6112/landau/service/ApplicationCommandService.kt
@@ -5,6 +5,8 @@ import de.tobi6112.landau.domain.command.core.AbstractCommand
 import discord4j.core.`object`.entity.ApplicationInfo
 import discord4j.rest.service.ApplicationService
 import mu.KotlinLogging
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 /**
  * Application command service
@@ -14,94 +16,60 @@ import mu.KotlinLogging
  */
 class ApplicationCommandService(
     private val applicationInfo: ApplicationInfo,
-    private val applicationService: ApplicationService
+    private val applicationService: ApplicationService,
+    private val config: CommandsConfig
 ) {
   private val logger = KotlinLogging.logger {}
   private val applicationId = applicationInfo.id.asLong()
 
   /**
-   * Create application Commands based on the provided configuration.
+   * Create commands and return their associated ID as well as the command
    *
-   * @param commands available commands
-   * @param config commands config
-   * @return map id <-> Command
+   * @param commands iterable of commands to create
+   * @return reactive tuples of their associated id and the command
    */
-  fun createCommands(
-      commands: Iterable<AbstractCommand>,
-      config: CommandsConfig
-  ): Map<Long, AbstractCommand> {
-    // Get global enabled commands
-    val globalEnabledCommands = commands.filter { config.global[it.name]?.enabled ?: true }
-    val globalCommands = createGlobalCommands(globalEnabledCommands)
+  fun createCommands(commands: Iterable<AbstractCommand>) =
+      Flux.merge(
+          this.createGlobalCommandsReactive(commands), this.createGuildCommandsReactive(commands))
 
-    // Get guild enabled commands
-    val guildEnabledCommands =
-        config.guilds.mapValues { (_, cmdCfg) ->
-          commands.filter { cmdCfg[it.name]?.enabled ?: true }
-        }
-    val guildCommands: MutableMap<Long, AbstractCommand> = mutableMapOf()
-    guildEnabledCommands.forEach { guildCommands.putAll(createGuildCommands(it.key, it.value)) }
+  private fun createGlobalCommandsReactive(commands: Iterable<AbstractCommand>) =
+      Flux.fromIterable(commands)
+          .filterWhen { isGlobalCommandEnabled(it) }
+          .flatMap { this.createGlobalCommandReactive(it) }
+          .map { cmd -> Pair(cmd.id().toLong(), commands.find { cmd.name() == it.name }!!) }
 
-    guildCommands.putAll(globalCommands)
-    return guildCommands.toMap()
-  }
+  private fun isGlobalCommandEnabled(command: AbstractCommand) =
+      Mono.just(config.global[command.name]?.enabled ?: true)
 
-  @Suppress("LOCAL_VARIABLE_EARLY_DECLARATION")
-  private fun createGlobalCommands(
-      commands: Iterable<AbstractCommand>
-  ): Map<Long, AbstractCommand> {
-    checkMultipleCommands(commands)
+  private fun createGlobalCommandReactive(command: AbstractCommand) =
+      this.applicationService
+          .createGlobalApplicationCommand(applicationId, command.toRequest())
+          .doOnSuccess { logger.info { "Successfully created global command $command" } }
+          .doOnError { err -> logger.error(err) { "Could not create global command $command" } }
 
-    val map: MutableMap<Long, AbstractCommand> = mutableMapOf()
+  private fun createGuildCommandsReactive(commands: Iterable<AbstractCommand>) =
+      Flux.fromIterable(config.guilds.entries).flatMap { map ->
+        this.createGuildCommandsReactive(
+            map.key, commands.filter { map.value.keys.contains(it.name) })
+      }
 
-    // TODO Return Mono
-    commands.distinctBy { it.name }.forEach {
-      val request = it.toRequest()
-      val res =
-          this.applicationService
-              .createGlobalApplicationCommand(applicationId, request)
-              .doOnError { err -> logger.error(err) { "Could not create global command $it" } }
-              .doOnSuccess { logger.info { "Successfully created global command $it" } }
-              .block()
+  private fun createGuildCommandsReactive(guildId: Long, commands: Iterable<AbstractCommand>) =
+      Flux.fromIterable(commands)
+          .filterWhen { isGuildCommandEnabled(guildId, it) }
+          .flatMap { this.createGuildCommandReactive(guildId, it) }
+          .map { cmd -> Pair(cmd.id().toLong(), commands.find { cmd.name() == it.name }!!) }
 
-      map[res!!.id().toLong()] = it
-    }
+  private fun isGuildCommandEnabled(guildId: Long, command: AbstractCommand) =
+      Mono.just(config.guilds?.get(guildId)?.get(command.name)?.enabled ?: false)
 
-    return map
-  }
-
-  private fun createGuildCommands(
-      guildId: Long,
-      commands: Iterable<AbstractCommand>
-  ): Map<Long, AbstractCommand> {
-    checkMultipleCommands(commands)
-
-    val map: MutableMap<Long, AbstractCommand> = mutableMapOf()
-
-    // TODO Return Mono
-    commands.distinctBy { it.name }.forEach { command ->
-      val request = command.toRequest()
-      val res =
-          this.applicationService
-              .createGuildApplicationCommand(applicationId, guildId, request)
-              .doOnError { err ->
-                logger.error(err) { "Could not create guild command $command on guild $guildId" }
-              }
-              .doOnSuccess {
-                logger.info { "Successfully created global command $command on guild $guildId" }
-              }
-              .block()
-
-      map[res!!.id().toLong()] = command
-    }
-
-    return map
-  }
-
-  private fun checkMultipleCommands(commands: Iterable<AbstractCommand>) {
-    commands.groupingBy { it.name }.eachCount().filter { it.value > 1 }
-        .forEach { (name, count) ->
-          logger.warn { "Multiple commands for name $name found ($count times)" }
-        }
-  }
+  private fun createGuildCommandReactive(guildId: Long, command: AbstractCommand) =
+      this.applicationService
+          .createGuildApplicationCommand(applicationId, guildId, command.toRequest())
+          .filterWhen { isGuildCommandEnabled(guildId, command) }
+          .doOnSuccess {
+            logger.info { "Successfully created global command $command on guild $guildId" }
+          }
+          .doOnError { err ->
+            logger.error(err) { "Could not create guild command $command on guild $guildId" }
+          }
 }
